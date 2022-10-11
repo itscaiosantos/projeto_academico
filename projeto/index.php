db1bb4353cac5a0f3c8af86558f1f930991ea08d
itscaiosantos <83199605+itscaiosantos@users.noreply.github.com>
2022-10-11 12:17:21 -0500

    ....

---
+++ projeto/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <style>
+        body{
+           font-family: Arial, Helvetica, sans-serif;
+           background-image: linear-gradient(45deg, purple, cyan);
+           text-align: center;
+           color: white;
+
+        }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 30px;
+            border-radius: 10px;
+        }
+        a{
+            text-decoration: none;
+            color: white;
+            border-radius: 10px;
+            padding: 10px;
+            border: 3px solid dodgerblue;
+        }
+        a:hover{
+            background-color: dodgerblue;
+
+        }
+    </style>
+</head>
+<body>
+    <h1>Prova dia 18/10/2022</h1>
+    <h2>Estudem html, css, php e mySql</h2>
+
+    <div class="box">
+        <a href="tela_login.html">Login</a>
+        <a href="cadastro.php">Cadastre-se</a>
+    </div>
+    
+</body>
+</html>
